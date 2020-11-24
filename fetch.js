67c4b669220eb97bf5760c2b69341b782136a676
Alade <alade.faruk11@gmail.com>
2020-11-24 13:31:53 +0100

    Emergency Numbers-commit 1

--- fetch.js
+++ fetch.js
@@ -1,4 +1,6 @@
 let loader= `<h1 class='load'>LOADING...</h1>`
+let input=document.querySelector('.input');
+document.querySelector('#app').insertAdjacentHTML('afterbegin',loader)
 
 const countryData=(array)=>{
     let emajencyCard=array.map(num=>{
@@ -6,32 +8,50 @@ const countryData=(array)=>{
     <div class='card'>
     <a class='button' href='tel: ${num.number}'>Call</a>
     <p>${num.name}</p>
-    
     </div>`;
 }).join('')
+
 return emajencyCard
 }
 
 
+let emergencyCall= async () =>{
+    const response = await fetch("https://emajency.com/js/numbers.json");
+    if(!response.ok){
+    const errorMessage= `Something went wrong:  ${response.status} error`;
+    throw new Error(errorMessage)
+    }
 
-document.querySelector('#app').insertAdjacentHTML('afterbegin',loader)
-
-
-fetch("https://emajency.com/js/numbers.json")
-.then(response=>{
-    if (response.ok){
-    return response.json();
-} else {
-    return Promise.reject(response);
+    const numbers= await response.json();
+    return numbers
 }
-}).then(data=>{
-    console.log(data);
+
+
+emergencyCall()
+.then(data=>{
     document.querySelector('.load').remove()
     document.querySelector('#app').insertAdjacentHTML('afterbegin',countryData(data))
-}).catch (err=>{
-    console.log(err)
-    window.alert(`Something went wrong: ${err.status} error`)
+
+})
+.catch (err=>{
+    window.alert( `Something went wrong:  ${err.status} error`)
     document.querySelector('.load').remove()
     let errr= `<h1 class='error'> Something went wrong:  ${err.status} error</h1>`
     document.querySelector('#app').insertAdjacentHTML('afterbegin',errr)
+})
+
+
+
+
+
+input.addEventListener("keyup", (event)=>{
+    let search=event.target.value
+    document.querySelector('#app').innerHTML="";
+
+    emergencyCall()
+    .then(data=>{
+            const filterednames=data.filter(items=> (items.name.toLowerCase().includes(search.toLowerCase())))
+            document.querySelector('#app').insertAdjacentHTML('afterbegin',countryData(filterednames))
+
+});
 })
